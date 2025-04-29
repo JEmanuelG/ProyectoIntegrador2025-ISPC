e4014754c37033bd6ee844a0153c58ddc2458f32
Emanuel Guaráz <guarazjemanuel@gmail.com>
2025-04-28 23:19:11 -0300

    Resuelvo actividad N2

---
+++ DiagnosticoInicial/actividad2.js
@@ -0,0 +1,15 @@
+const persona = {
+    nombre: "Lucía",
+    edad: 28,
+    profesion: "Diseñadora",
+    };
+
+
+// Muestra por consola el msj solicitado
+console.log(`${persona.nombre} tiene ${persona.edad} años y trabaja 
+como ${persona.profesion}.`)
+
+
+
+// agrega una nueva propiedad al objeto
+persona.ciudad = "Rosario"
